a10e442c0c02c9967cfd2f55a0f77ce4c7619729
Piyush Mehta <me@piyushmehta.com>
2021-01-26 16:02:09 +0530

    fixes and setup

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,21 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+import 'reflect-metadata';
+import express from 'express';
+import morgan from 'morgan';
+import { createConnection } from 'typeorm';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (_, res) => res.send('Hello'));
+app.listen(5500, async () => {
+    console.log('Server running at Port http://localhost:5500');
+
+    try {
+        await createConnection();
+        console.log('Database Connected!');
+    } catch (err) {
+        console.log(err);
+    }
+});
