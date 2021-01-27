f2a820471bbb7ae69a42c6c459d54e188bdf7127
Piyush Mehta <me@piyushmehta.com>
2021-01-27 22:32:20 +0530

    comments and fetching posts api

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -3,6 +3,7 @@ import Post from '../entities/Post';
 
 import auth from '../middleware/auth';
 import Sub from '../entities/Sub';
+import Comment from '../entities/Comment';
 
 const createPost = async (req: Request, res: Response) => {
     const { title, body, sub } = req.body;
@@ -40,7 +41,7 @@ const getPosts = async (_: Request, res: Response) => {
 const getPost = async (req: Request, res: Response) => {
     const { identifier, slug } = req.params;
     try {
-        const posts = await Post.findOneOrFail({ identifier, slug });
+        const posts = await Post.findOneOrFail({ identifier, slug }, { relations: ['sub'] });
 
         return res.json(posts);
     } catch (error) {
@@ -48,10 +49,32 @@ const getPost = async (req: Request, res: Response) => {
         return res.status(500).json({ error: 'Something went wrong' });
     }
 };
+
+const commentOnPost = async (req: Request, res: Response) => {
+    const { identifier, slug } = req.params;
+    const body = req.body.body;
+
+    try {
+        const post = await Post.findOneOrFail({ identifier, slug });
+        const comment = new Comment({
+            body,
+            user: res.locals.user,
+            post,
+        });
+
+        await comment.save();
+
+        return res.json(comment);
+    } catch (error) {
+        console.error(error);
+        return res.status(404).json({ error: 'Post not found' });
+    }
+};
+
 const router = Router();
 
 router.post('/', auth, createPost);
 router.get('/', getPosts);
 router.get('/:identifier/:slug', getPost);
-
+router.post('/:identifier/:slug/comments', auth, commentOnPost);
 export default router;
