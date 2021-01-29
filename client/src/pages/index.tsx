6d5978f95e0d68c00c3a8d22733ab282850ecbc4
Piyush Mehta <me@piyushmehta.com>
2021-01-29 16:17:45 +0530

    Posts Skeleton

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,30 @@
 import Head from "next/head";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { Post } from "../types";
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  useEffect(() => {
+    axios
+      .get("/posts")
+      .then((res) => setPosts(res.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div>
+    <div className="pt-12">
       <Head>
         <title>Social Shout</title>
       </Head>
+      <div className="container flex pt-4">
+        <div className="w-160">
+          {posts.map((post) => (
+            <div key={post.identifier}></div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -24,9 +24,23 @@ module.exports = {
         160: "40rem",
       },
     },
+    container: false,
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          width: "100%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          "@screen sm": { maxWidth: "640px" },
+          "@screen md": { maxWidth: "768px" },
+          "@screen lg": { maxWidth: "975px" },
+        },
+      });
+    },
+  ],
 };
