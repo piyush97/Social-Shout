4f2f51e3bdd6debd723c9951e5030919796b2131
Piyush Mehta <me@piyushmehta.com>
2021-01-29 15:09:48 +0530

    navbar

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,30 +1,39 @@
-import React from "react";
-import styles from "../styles/Home.module.css";
-import Head from "next/head";
 import Link from "next/link";
 
 import Logo from "../images/logo.svg";
 
-const index = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Social Shout</title>
-      </Head>
-      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 bg-white">
-        <div className="flex items-center">
-          <Link href="/">
-            <a>
-              <Logo className="mr-2 w-9 h-9" />
-            </a>
-          </Link>
-          <span className="text-2xl font-semibold">
-            <Link href="/">Social Shout</Link>
-          </span>
-        </div>
-      </div>
+const Navbar: React.FC = () => (
+  <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
+    {/* Logo and title */}
+    <div className="flex items-center">
+      <Link href="/">
+        <a>
+          <Logo className="w-8 h-8 mr-2" />
+        </a>
+      </Link>
+      <span className="text-2xl font-semibold">
+        <Link href="/">Social Shout</Link>
+      </span>
     </div>
-  );
-};
+    {/* Serach Input */}
+    <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+      <i className="pl-4 pr-3 text-gray-500 fas fa-search "></i>
+      <input
+        type="text"
+        className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+        placeholder="Search"
+      />
+    </div>
+    {/* Auth buttons */}
+    <div className="flex">
+      <Link href="/login">
+        <a className="w-32 py-1 mr-4 leading-5 hollow blue button">log in</a>
+      </Link>
+      <Link href="/register">
+        <a className="w-32 py-1 leading-5 blue button">sign up</a>
+      </Link>
+    </div>
+  </div>
+);
 
-export default index;
+export default Navbar;
