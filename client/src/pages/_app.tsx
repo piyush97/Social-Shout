bbf2324fbce952becb1d89e937483cf2ec615e6a
Piyush Mehta <me@piyushmehta.com>
2021-01-29 15:38:14 +0530

    Navbar modularize

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import axios from "axios";
 import { AppProps } from "next/app";
+
 import "../styles/tailwind.css";
+
+import Navbar from "../components/Navbar";
+import { Fragment } from "react";
 
 axios.defaults.baseURL = "http://localhost:5500/api";
 axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Navbar />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default App;
