814f3e3b1e9f30975ed0ecea5bf1e13175131de3
Piyush Mehta <me@piyushmehta.com>
2021-01-29 12:54:46 +0530

    Sign Up made and login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -7,7 +7,6 @@ import { useRouter } from "next/router";
 import InputGroup from "../components/inputGroup";
 
 export default function Home() {
-  const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [agreement, setAgreement] = useState(false);
@@ -18,19 +17,13 @@ export default function Home() {
   const submitForm = async (event: FormEvent) => {
     event.preventDefault();
 
-    if (!agreement) {
-      setErrors({ ...errors, agreement: "You must agree to T&Cs" });
-      return;
-    }
-
     try {
-      await axios.post("/auth/register", {
-        email,
-        password,
+      await axios.post("/auth/login", {
         username,
+        password,
       });
 
-      router.push("/login");
+      router.push("/");
     } catch (error) {
       console.error(error);
       setErrors(error.response.data);
@@ -40,7 +33,7 @@ export default function Home() {
   return (
     <div className="flex">
       <Head>
-        <title>Register</title>
+        <title>Login</title>
       </Head>
 
       <div
@@ -49,34 +42,11 @@ export default function Home() {
       ></div>
       <div className="flex flex-col justify-center pl-6">
         <div className="w-70">
-          <h1 className="mb-2 text-lg font-medium">Sign Up</h1>
+          <h1 className="mb-2 text-lg font-medium">Login</h1>
           <p className="mb-10 text-xs">
             By continuing, you agree to our User Agreement and Privacy Policy
           </p>
           <form onSubmit={submitForm}>
-            <div className="mb-6">
-              <input
-                type="checkbox"
-                className="mr-1 cursor-pointer"
-                id="agreement"
-                checked={agreement}
-                onChange={(e) => setAgreement(e.target.checked)}
-              />
-              <label htmlFor="agreement" className="text-xs cursor-pointer">
-                I agree to get emails about cool stuff on Social Shout
-              </label>
-              <small className="block font-medium text-red-600">
-                {errors.agreement}
-              </small>
-            </div>
-            <InputGroup
-              className="mb-2"
-              value={email}
-              setValue={setEmail}
-              placeholder="Email"
-              error={errors.email}
-              type="email"
-            />
             <InputGroup
               className="mb-2"
               value={username}
@@ -94,13 +64,13 @@ export default function Home() {
               type="password"
             />
             <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded bg-blue500">
-              Sign Up
+              Log in
             </button>
           </form>
           <small>
-            Already a Social Shouter?{" "}
-            <Link href="/login">
-              <a className="ml-1 text-blue-500 uppercase">Login</a>
+            New to Social Shouter?{" "}
+            <Link href="/register">
+              <a className="ml-1 text-blue-500 uppercase">Sign Up</a>
             </Link>
           </small>
         </div>
