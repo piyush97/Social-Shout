340ebc42999904e70e65f0569d4a963d97c7c7c2
Piyush Mehta <me@piyushmehta.com>
2021-01-29 15:40:48 +0530

    Navbar not in authroutes

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,14 +5,18 @@ import "../styles/tailwind.css";
 
 import Navbar from "../components/Navbar";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 axios.defaults.baseURL = "http://localhost:5500/api";
 axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
   return (
     <Fragment>
-      <Navbar />
+      {!authRoute && <Navbar />}
       <Component {...pageProps} />
     </Fragment>
   );
